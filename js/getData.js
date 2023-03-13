2d4f135700d11cc0fc5272414a398ffc146a2b61
Olga Chernega <eliang.cheng@gmail.com>
2023-03-13 17:58:47 +0200

    feat: add server files

--- js/getData.js
+++
@@ -1,7 +0,0 @@
-export const getData = async url => {
-  const response = await fetch(url);
-
-  if(response.ok) {
-    return response.json()
-  }
-}
--- js/key.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = "https://burger-service-api.glitch.me/";
-export const PRODUCT_PREFIX = "/api/product";
